56a51e3742434299c19da9c6e18c6f21375a7688
dylanbyars <dylanbyars@system76-pc.localdomain>
2021-10-14 15:28:37 -0400

    raw and pretty version + change style on print

--- pages/resume/index.page.tsx
+++ pages/resume/index.page.tsx
@@ -1,31 +1,80 @@
+import classNames from "classnames"
 import fs from "fs"
 import { NextPage } from "next"
 import { join } from "path"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { remark } from "remark"
-import html from "remark-html"
+import markdown2html from "remark-html"
 import PageContainer from "../../components/PageContainer"
 
-const Resume: NextPage<{ content: string }> = ({ content }) => (
-  <PageContainer title="Resume · Dylan Byars">
-    <article
-      className="prose lg:prose-lg lg:max-w-3xl font-mono mx-auto mt-6 mb-10 px-4 md:px-0 md:mt-8"
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
-  </PageContainer>
-)
+const useDetectPrint = () => {
+  const [isPrinting, setIsPrinting] = useState(false)
+
+  const handleBeforeprint = () => setIsPrinting(true)
+  const handleAfterprint = () => setIsPrinting(false)
+
+  useEffect(() => {
+    window.addEventListener("beforeprint", handleBeforeprint)
+    window.addEventListener("afterprint", handleAfterprint)
+    return () => {
+      window.removeEventListener("beforeprint", handleBeforeprint)
+      window.removeEventListener("afterprint", handleAfterprint)
+    }
+  })
+
+  return isPrinting
+}
+
+const Resume: NextPage<{ html: string; raw: string }> = ({ html, raw }) => {
+  const [showRaw, setShowRaw] = useState(false)
+  const isPrinting = useDetectPrint()
+
+  useEffect(() => {
+    setShowRaw(isPrinting)
+  }, [isPrinting])
+
+  return (
+    <PageContainer title="Resume · Dylan Byars">
+      {!isPrinting && (
+        <button
+          className="float-right mr-8"
+          onClick={() => setShowRaw(!showRaw)}
+        >
+          {showRaw ? "pretty" : "raw"}
+        </button>
+      )}
+      {showRaw ? (
+        <article className={classNames({ "m-3": !isPrinting })}>
+          {raw.split("\n").map((line) => (
+            <div
+              className={classNames({
+                "my-1": line === "",
+                "text-lg": /^#/.test(line),
+              })}
+            >
+              {line}
+            </div>
+          ))}
+        </article>
+      ) : (
+        <article
+          className="prose md:max-w-3xl font-mono mx-auto mt-6 mb-10 px-4 md:px-0 md:mt-8"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      )}
+    </PageContainer>
+  )
+}
 
 export async function getStaticProps() {
-  const content = await remark()
-    .use(html)
-    .process(
-      fs.readFileSync(
-        join(process.cwd(), "pages/resume/Dylan_Byars_Resume.md"),
-        "utf8"
-      )
-    )
+  const resumeFile = fs.readFileSync(
+    join(process.cwd(), "pages/resume/Dylan_Byars_Resume.md"),
+    "utf8"
+  )
 
-  return { props: { content: content.toString() } }
+  const virtualFile = await remark().use(markdown2html).process(resumeFile)
+
+  return { props: { html: virtualFile.toString(), raw: resumeFile } }
 }
 
 export default Resume
